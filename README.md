ff9f03146b8f2ff3794c4b26204322af346cbd0e
alexandergogl <gogl.alexander@gmail.com>
2015-09-11 16:43:21 +0200

    Added an relative date replacement method
    I have rewritten the workflow in python (dumping applescript).

--- README.md
+++ README.md
@@ -1,37 +1,47 @@
 # An org-mode-capture workflow for Alfred
-Add a todo or a note to an org-mode file with a short and convenient command in [Alfred](https://www.alfredapp.com/). The workflow requires Alfred's "Powerpack."
+Append a todo or a note to an org-mode file with a short and convenient command in [Alfred](https://www.alfredapp.com/). The workflow requires Alfred's "Powerpack."
 
 ## Features
-The command appends a second level heading to a user defined .org file and puts all what follows `::` into the body of the heading (see figures below).
-
+The command appends a second level heading to a user defined .org file and puts all what follows `:: ` into the body of the heading (see figures below).
 
 Type todo to add a todo:
 
-![Capture a todo](images/image-03.png)
+![Capture a todo](images/todo-capture.png)
 
-![Capture a todo](images/image-01.png)
+![Capture a todo](images/todo-notification.png)
 
 
 Type note to add a note:
 
-![Capture a note](images/image-04.png)
+![Capture a note](images/note-capture.png)
 
-![Capture a note](images/image-02.png)
+![Capture a note](images/note-notification.png)
 
 
 The added notes are divided into title and content:
 
-![Capture a note](images/image-05.png)
+![Capture a note](images/result.png)
+
+
+Relative dates (Monday, tuesday, tomorrow, morgen, freitag) in the body of the entry are converted into orgmode specific date formats `<2015-09-11 Fri>`.
+
+![Relative dates in Alfred](images/date_replacement-01.png)
+
+![become orgmode dates](images/date_replacement-02.png)
 
 
 ## Installation
-Double klick on `org-mode-capture.alfredworkflow` to add it to Alfred's set of workflows. Then you need to edit the default path to your org-mode inbox file in both applescript nodes (absolute notation of the path is necessary):
+Double klick on `org-mode-capture.alfredworkflow` to add it to Alfred's set of workflows. Then you need to set the path to your existing org-mode inbox file in both python script nodes (absolute notation of the path is necessary). You can also change the delimiter pattern to distinguish between head and body elements:
 
-```applescript
-set inboxFile to "Inbox.org"
-set inboxFilePath to "/Users/Alex/Documents/"
+```python
+org.filename = '/Users/Alex/Documents/Planung/Planning/Inbox.org'
+org.delimiter = ':: '
 ```
+
+![Edit the python script nodes within Alfred](images/workflow.png)
+
+## Supported modes of notification
 
 If you don't use growl, you can change the notification nodes in the alfred workflows to apple's notification centre:
 
-![Notification system](images/image-06.png)
+![Notification system](images/supported_notificaitons.png)
---
+++ source/orgmode_entry.py
@@ -0,0 +1,112 @@
+import datetime
+import re
+
+
+class OrgmodeEntry(object):
+    """Format a generic heading with an optional body into an orgmode file.
+
+    Convert relative dates into orgmode date strings.
+    """
+    def __init__(self):
+        self.date_format = "<%s-%s-%s %s>"
+        self.weekdays = {
+            "montag": 0,
+            "monday": 0,
+            "dienstag": 1,
+            "tuesday": 1,
+            "mittwoch": 2,
+            "wednesday": 2,
+            "donnerstag": 3,
+            "thursday": 3,
+            "freitag": 4,
+            "friday": 4,
+            "samstag": 5,
+            "saturday": 5,
+            "sonntag": 6,
+            "sunday": 6
+        }
+        self.relative_dates = {
+            "heute": 0,
+            "today": 0,
+            "morgen": 1,
+            "tomorrow": 1
+        }
+        self.filename = "~/Desktop/Inbox.org"
+        self.delimiter = ":: "
+        self.message_format = "Added '%s \n %s' to %s."
+
+    def create_entry(self, string):
+        entry = self.format_entry(string)
+        self.write_to_file(entry)
+        message = self.create_message()
+        return message
+
+    def write_to_file(self, string):
+        with open(self.filename, "a") as myfile:
+            myfile.write(string)
+        pass
+
+    def format_entry(self, string):
+        heading, body = self.split_string(string)
+        self.heading = heading
+        heading = "\n** " + heading
+        body = self.replace_date(body)
+        self.body = body
+        entry = heading + "\n" + body
+        return entry
+
+    def split_string(self, string):
+        return string.split(self.delimiter)
+
+    def replace_date(self, string):
+        dict_keys = '|'.join(self.weekdays.keys()) + "|" + '|'.join(self.relative_dates.keys())
+        expression = r'\b(' + dict_keys + r')\b'
+        pattern = re.compile(expression, re.IGNORECASE)
+        string = pattern.sub(lambda x: self.convert_date(x.group()), string)
+        return string
+
+    def convert_date(self, string):
+        today = datetime.datetime.now()
+        delta = self.convert_relative_date(string)
+        date = today + datetime.timedelta(days=delta)
+        date = self.format_date(date)
+        return date
+
+    def convert_relative_date(self, string):
+        # string dictionaries with delta day value
+        string = string.lower()
+        relative_dates = self.relative_dates
+        weekdays = self.weekdays
+
+        if string in relative_dates:
+            delta = relative_dates[string]
+        elif string in weekdays:
+            today = datetime.datetime.today()
+            current = today.weekday()
+            weekday = weekdays[string]
+            if current == weekday:
+                delta = 7
+            elif weekday < current:
+                delta = 7 - (current - weekday)
+            else:
+                delta = weekday - current
+        else:
+            delta = False
+        return delta
+
+    def format_date(self, date):
+        year = date.strftime("%Y")
+        month = date.strftime("%m")
+        day = date.strftime("%d")
+        weekday = date.strftime("%a")
+
+        date = self.date_format % (year, month, day, weekday)
+        return date
+
+    def create_message(self):
+        # Get filename of file path
+        filepath = self.filename.split('/')
+        filename = filepath[len(filepath) - 1]
+
+        message = self.message_format % (self.heading, self.body, filename)
+        return message
